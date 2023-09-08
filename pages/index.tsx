7740e13f89d06a54001eb2f977b80b855cb9739d
Inome <inome@bemarkable.co.kr>
2023-09-08 15:19:17 +0900

    업데이트

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import {useEffect, useState} from 'react';
+
+export default function Page() {
+    const [thisPort, setThisPort] = useState(0)
+    useEffect(() => {
+        setThisPort(3005);
+        return () => {
+          console.log('a');
+        }
+      }, [])
+    return (
+        <div className="App">
+            <h1>Hello, Next.js!{thisPort}</h1>
+        </div>
+    )
+}
