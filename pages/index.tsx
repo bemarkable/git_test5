c7d8875152af604c56ab1e7ce92268c8a4f94ce0
Inome <inome@bemarkable.co.kr>
2023-09-13 15:42:29 +0900

    change

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,21 @@
-import {useEffect, useState} from 'react';
+import * as React from 'react';
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 
-export default function Page() {
-    const [thisPort, setThisPort] = useState(0)
-    useEffect(() => {
-        setThisPort(3005);
-        return () => {
-          console.log('a');
-        }
-      }, [])
-    return (
-        <div className="App">
-            <h1>Hello, Next.js!-{thisPort}-123123123</h1>
-        </div>
-    )
+const rows: GridRowsProp = [
+  { id: 1, col1: 'Hello', col2: 'World' },
+  { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+  { id: 3, col1: 'MUI', col2: 'is Amazing' },
+];
+
+const columns: GridColDef[] = [
+  { field: 'col1', headerName: 'Column 1', width: 150 },
+  { field: 'col2', headerName: 'Column 2', width: 150 },
+];
+
+export default function App() {
+  return (
+    <div style={{ height: 300, width: '100%' }}>
+      <DataGrid rows={rows} columns={columns} />
+    </div>
+  );
 }
